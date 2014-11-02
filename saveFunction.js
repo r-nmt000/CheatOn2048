7cfefa6dd66b08c9944894cfe5956b7ef43c8ae9
Ryusuke Nomoto <r.nmt000@gmail.com>
2014-11-02 13:56:23 +0900

    complete implementing savefunc

--- saveFunction.js
+++ saveFunction.js
@@ -1,22 +1,22 @@
 function getTile(elem,grid) {
   var xy = elem.attr('class').match(/tile-position-(\d)-(\d)/).slice(1);
   var position = {};
-  position['x'] = number(xy[0])-1;
-  position['y'] = number(xy[1])-1;
-  var value = number(elem.children().text());
+  position['x'] = Number(xy[0])-1;
+  position['y'] = Number(xy[1])-1;
+  var value = Number(elem.children().text());
   var tile = new Tile(position, value);
-  grid.inserttile(tile);
+  grid.insertTile(tile);
 }
 function getGrid(grid) {
   $('.tile-container').children().each(function(){getTile($(this),grid)});
 }
-function getAll2048data() {
+function getAll2048Data() {
   console.log('getall2048data');
   var score = $('.score-container').val();
   var grid  = new Grid(size=4);
   getGrid(grid);
-  localstorage.score = json.stringify(score);
-  localstorage.grid  = json.stringify(grid);
+  localStorage.score = JSON.stringify(score);
+  localStorage.grid  = JSON.stringify(grid);
 }
 console.log('savefunc');
 getAll2048Data();
